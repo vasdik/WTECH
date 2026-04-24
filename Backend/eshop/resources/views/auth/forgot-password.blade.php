94c01c1139629b69619047acd9c587edbd1397f2
xskrabakj <xskrabakj@stuba.sk>
2026-04-24 19:19:39 +0200

    minor changes

--- Backend/eshop/resources/views/auth/forgot-password.blade.php
+++ Backend/eshop/resources/views/auth/forgot-password.blade.php
@@ -1,4 +1,8 @@
-<x-guest-layout>
+@extends('layouts.shop-auth')
+
+@section('title', 'S&J - Register')
+
+@section('content')
     <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
         {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
     </div>
@@ -22,4 +26,4 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+@endsection
